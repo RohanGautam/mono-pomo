771188cf30b820623433ff769e0ac20a8203eae4
RohanGautam <rohan1gautam@gmail.com>
2020-12-29 22:13:07 +0800

    add todos

--- packages/web/src/components/Timer.tsx
+++ packages/web/src/components/Timer.tsx
@@ -4,10 +4,13 @@ import { useTimer, formatTime, useEffectOnlyOnce } from "@mono-pomo/common";
 import React from "react";
 import Head from "next/head";
 
+// todo: ring a sound and do stuff to get attention on expire
+// tell this what the work type is so it can update the head
+
 export const Timer = ({ expiryTime }: { expiryTime: number }) => {
   const { seconds, minutes, isRunning, resume, pause } = useTimer({
     expiryTimestamp: expiryTime,
-    onExpire: () => alert("timer done!"), // todo: ring a sound and do stuff to get attention
+    onExpire: () => alert("timer done!"),
   });
   //   this will run only once, when the widget is first created.
   // it is created every time expirytime changes, as we are giving it a new key.
