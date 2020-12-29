f0e4835fc60736448544c21f1608f2db3a5648ff
RohanGautam <rohan1gautam@gmail.com>
2020-12-29 22:11:49 +0800

    show time in the document title

--- packages/web/src/components/Timer.tsx
+++ packages/web/src/components/Timer.tsx
@@ -2,11 +2,12 @@ import { TimerText } from "../components/TimerText";
 import { TimerControlButton } from "../components/TimerControlButton";
 import { useTimer, formatTime, useEffectOnlyOnce } from "@mono-pomo/common";
 import React from "react";
+import Head from "next/head";
 
 export const Timer = ({ expiryTime }: { expiryTime: number }) => {
   const { seconds, minutes, isRunning, resume, pause } = useTimer({
     expiryTimestamp: expiryTime,
-    onExpire: () => console.warn("timer done!"),
+    onExpire: () => alert("timer done!"), // todo: ring a sound and do stuff to get attention
   });
   //   this will run only once, when the widget is first created.
   // it is created every time expirytime changes, as we are giving it a new key.
@@ -19,6 +20,9 @@ export const Timer = ({ expiryTime }: { expiryTime: number }) => {
   };
   return (
     <React.Fragment>
+      <Head>
+        <title>{formatTime(minutes, seconds)}</title>
+      </Head>
       <TimerText displayTime={formatTime(minutes, seconds)} />
       <TimerControlButton
         showStart={!isRunning}
